e27c856772cd8ef253f8c71a453514b0f99e55ff
GalVeks <74971131+GalVeks@users.noreply.github.com>
2022-11-09 14:52:00 +0200

    Test RDS

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,46 @@
-import pandas as pd
 import json
-import numpy as np
-import requests
+    
+    
+import psycopg2
+from datetime import date
+import datetime
+
 
 def lambda_handler(event, context):
-    pandas_version = pd.__version__
-    numpy_version = np.__version__
+    # TODO implement
 
-    r = requests.get("https://www.google.com")
-    google_status_code = r.status_code
+    
+    connection = psycopg2.connect(user="postgres",
+                                  password="postgres",
+                                  host="devpostgres01.cphbkxf9hicq.us-east-1.rds.amazonaws.com",
+                                  port="5432",
+                                  database="postgres")
+    cursor = connection.cursor()
 
-    return_statement = 'Pandas Version: ', pandas_version, 'Numpy Version: ', numpy_version, 'Google Status Code: ', google_status_code
+    postgres_insert_query = """ INSERT INTO "SP_ADMIN".ref_runbook_history (RUN_ID, 
+                                                            CATEGORY, 
+                                                            DISPLAY_NAME, 
+                                                            AS_OF_DATE, 
+                                                            START_TIME, 
+                                                            END_TIME, 
+                                                            STATUS, 
+                                                            DURATION) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
+    record_to_insert = ('1', 
+                        'Instagram', 
+                        'DISPLAY_NAME',
+                        date.today(),
+                        datetime.datetime.now()
+                        ,datetime.datetime.now()
+                        ,'Failed'
+                        ,datetime.datetime.now() - datetime.datetime.now())
+    cursor.execute(postgres_insert_query, record_to_insert)
 
+    connection.commit()
+    count = cursor.rowcount
+    print(count, "Record inserted successfully into mobile table")
+
+   
     return {
-        'statusCode':200,
-        'body': json.dumps(return_statement)
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
     }
