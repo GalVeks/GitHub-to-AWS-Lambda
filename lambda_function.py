4a7998395a7bcd461b0441b526c3b0671b61df5f
GalVeks <74971131+GalVeks@users.noreply.github.com>
2022-11-09 15:16:46 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,16 +5,9 @@ import numpy as np
 import requests
 
 def lambda_handler(event, context):
-    pandas_version = pd.__version__
-    numpy_version = np.__version__
-
-    r = requests.get("https://www.google.com")
-    google_status_code = r.status_code
-
-    return_statement = 'Pandas Version: ', pandas_version, 'Numpy Version: ', numpy_version, 'Google Status Code: ', google_status_code
+    print('a')
 
     return {
-        'statusCode':200,
-        'body': json.dumps(return_statement)
+        'statusCode':200
     }
 
