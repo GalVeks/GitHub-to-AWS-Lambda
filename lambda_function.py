11a77e80f7505efe0f985da18fb58fd45ed3e4a6
Gal Vekselman <galv@excelando.co.il>
2022-11-15 14:33:10 +0200

    GV: Demo Daniel

--- lambda_function.py
+++ lambda_function.py
@@ -12,8 +12,8 @@ import main
 def lambda_handler(event, context):
     # Create Postgres SQL connection (Parameters should be stored in KMS)
 
-    account_scrape = str(event['queryStringParameters']['Account'])
-    event_scrape = str(event['rawPath'])
+    account_scrape = "galvekselman"  # str(event['queryStringParameters']['Account'])
+    event_scrape = "/igFollowers"  # str(event['rawPath'])
 
     connection = psycopg2.connect(user="postgres",
                                   password="postgres",
@@ -25,8 +25,11 @@ def lambda_handler(event, context):
     # Get last Runbook id
     cursor.execute('''select coalesce (max(run_id) + 1 , 1)
                         from "SP_ADMIN".ref_runbook_history''')
+
     result = cursor.fetchone()
     idx = str(result[0])
+
+    run_timestamp = datetime.datetime.now()
 
     # Define insert query (to factorization later)
     postgres_insert_query = """ INSERT INTO "SP_ADMIN".ref_runbook_history (RUN_ID, 
@@ -36,39 +39,66 @@ def lambda_handler(event, context):
                                                             START_TIME, 
                                                             END_TIME, 
                                                             STATUS, 
-                                                            DURATION) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
+                                                            DURATION,
+                                                            PAYLOAD) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
 
     record_to_insert = (idx
                         , event_scrape
                         , account_scrape
                         , date.today()
-                        , datetime.datetime.now()
-                        , datetime.datetime.now()
-                        , 'Failed'
-                        , datetime.datetime.now() - datetime.datetime.now())
+                        , run_timestamp
+                        , run_timestamp
+                        , 'RUNNING'
+                        , run_timestamp - run_timestamp
+                        , 'RUNNING')
 
     # Commit query
     cursor.execute(postgres_insert_query, record_to_insert)
     connection.commit()
     count = cursor.rowcount
-    print(count, "Record inserted successfully into mobile table")
+    print(count, "Record inserted successfully into runbook table")
+
+    try:
+        ##########################################
+        # Put your code in this block
+        ##########################################
+
+        # Scrape follower by account
+        if event_scrape == "/igFollowers":
+            print("Start scraping follower of :" + account_scrape)
+            main.main_run(account_scrape)
+
 
     ##########################################
-    # Put your code in this block
-    ##########################################
 
-    # Scrape follower by account
-    if event_scrape == "/igFollowers":
-        print("Start scraping follower of :" + account_scrape)
-        main.main_run(account_scrape)
+    except Exception as e:
+
+        postgres_insert_query = """ UPDATE "SP_ADMIN".ref_runbook_history 
+                                    SET status = 'FAILED'
+                                    ,end_time = %s
+                                    ,duration = %s
+                                    ,payload = %s
+                                    WHERE run_id = %s """
+
+        record_to_insert = (datetime.datetime.now()
+                            , datetime.datetime.now() - run_timestamp
+                            , str(e)
+                            , idx
+                            )
+
+        # execute the query
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+        count = cursor.rowcount
+
+        print(count, "Record updated successfully into runbook table")
 
     ##########################################
 
     return {
         'statusCode': 200,
         'body': json.dumps('ok')  # 'Scrape account: {}'.format(account_scrape))
-      }
-
+    }
 
 
 
--- public_func.py
+++ public_func.py
@@ -1,10 +1,12 @@
 from io import StringIO  # python3; python2: BytesIO
 import boto3
+import pandas as pd
+import yfinance as yf
 
 
 def save_pd_csv_to_s3(bucket='mdm-social-project', df='', key=''):
-
     csv_buffer = StringIO()
     df.to_csv(csv_buffer)
     s3_resource = boto3.resource('s3')
     s3_resource.Object(bucket, key).put(Body=csv_buffer.getvalue())
+
